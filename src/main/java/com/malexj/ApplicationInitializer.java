09bc89c6aafe302b47682013f27621144c6901b0
MAlexj <alexmaximovj@gmail.com>
2015-12-21 14:48:21 +0200

    add to project MainController

---
+++ src/main/java/com/malexj/ApplicationInitializer.java
@@ -0,0 +1,15 @@
+package com.malexj;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletException;
+
+/**
+ * Created by malex on 18.12.15.
+ */
+public class ApplicationInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+
+    }
+}
---
+++ src/main/java/com/malexj/config/WebConfig.java
@@ -0,0 +1,7 @@
+package com.malexj.config;
+
+/**
+ * Created by malex on 18.12.15.
+ */
+public class WebConfig {
+}
