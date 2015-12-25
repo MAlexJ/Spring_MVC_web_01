74a76673820ad7805410ffbf98ae0b2846a93fbd
Alex <alexmaximovj@gmail.com>
2015-12-25 09:08:03 +0200

    Update CustomerDAO.java

--- src/main/java/com/malexj/dao/CustomerDAO.java
+++ src/main/java/com/malexj/dao/CustomerDAO.java
@@ -1,9 +1,37 @@
-package com.malexj.dao;
+package com.malexj.datasource;
 
-public interface CustomerDAO {
+import com.malexj.entity.Content;
+import com.malexj.entity.Section;
+import java.util.List;
+
+public interface PostgresJDBC {
     void connect();
 
     boolean close();
 
     boolean isConnected();
+
+    List<String> getTableNames();
+
+    List<Content> getContentList();
+
+    void setContentList(List<Content> var1);
+
+    void updateContent(Content var1);
+
+    void clearContent();
+
+    List<Section> getSectionList();
+
+    void setSectionList(List<Section> var1);
+
+    void updateSection(Section var1);
+
+    void clearSection();
+
+    List<Section> getListSectionForNameContent(String var1);
+
+    void clearContentSection();
+
+    void setConnectionSection(Content var1, List<Section> var2);
 }
