72a677f5d76f603b3449f9e4e3bf8b9f6663fd16
Alex <alexmaximovj@gmail.com>
2015-12-25 09:07:18 +0200

    Update PostgresJDBC.java

--- src/main/java/com/malexj/datasource/PostgresJDBC.java
+++ src/main/java/com/malexj/datasource/PostgresJDBC.java
@@ -1,4 +1,13 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package com.malexj.datasource;
+
+import com.malexj.entity.Content;
+import com.malexj.entity.Section;
+import java.util.List;
 
 public interface PostgresJDBC {
     void connect();
@@ -6,4 +15,28 @@ public interface PostgresJDBC {
     boolean close();
 
     boolean isConnected();
+
+    List<String> getTableNames();
+
+    List<Content> getContentList();
+
+    void setContentList(List<Content> var1);
+
+    void updateContent(Content var1);
+
+    void clearContent();
+
+    List<Section> getSectionList();
+
+    void setSectionList(List<Section> var1);
+
+    void updateSection(Section var1);
+
+    void clearSection();
+
+    List<Section> getListSectionForNameContent(String var1);
+
+    void clearContentSection();
+
+    void setConnectionSection(Content var1, List<Section> var2);
 }
